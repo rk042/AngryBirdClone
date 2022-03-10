970e8b8e987e786b8a1edff830ab62b206b59ec5
rathod ketan <rathodketan042@gmail.com>
2022-03-10 15:08:46 +0530

    gameplay complete , working on camera effect and sound

---
+++ Assets/Scripts/BallThrow.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallThrow : MonoBehaviour
+{
+    [SerializeField] List<GameObject> projectTileList=new List<GameObject>();
+    [SerializeField] float throwForce, throwForceProjectile,projectTileGravityScale;
+
+    Rigidbody2D rd;
+    Vector2 startPosition;
+    bool isShooting=false;
+
+    private void Awake()
+    {
+        rd=GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        foreach (var item in projectTileList)
+        {
+            // item.GetComponent<Renderer>().enabled=false;
+            item.transform.position=transform.position;
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetAxis("Fire1")==1)
+        {
+            if (!isShooting)
+            {
+                isShooting=true;
+                startPosition=Input.mousePosition;
+            }
+            else
+            {
+                for (int i = 0; i < projectTileList.Count/2; i++)
+                {
+                    projectTileList[i].GetComponent<Renderer>().enabled=true;
+                    projectTileList[i].transform.position=projectTilePath(transform.position,GetFroceProjectile(),i);
+                }
+            }
+        }
+        else if(isShooting)
+        {
+            rd.AddForce(GetFroce());
+            isShooting=false;
+            for (int i = 0; i < projectTileList.Count; i++)
+            {
+                projectTileList[i].GetComponent<Renderer>().enabled=false;
+            }
+        }
+    }
+
+    private Vector2 GetFroce()
+    {
+        return (new Vector2(startPosition.x,startPosition.y)- new Vector2(Input.mousePosition.x,Input.mousePosition.y)) * throwForce;
+    }
+    private Vector2 GetFroceProjectile()
+    {
+        return (new Vector2(startPosition.x,startPosition.y)- new Vector2(Input.mousePosition.x,Input.mousePosition.y)) * throwForceProjectile;
+    }
+
+    private Vector2 projectTilePath(Vector2 startPath,Vector2 velocity,int index)
+    {
+        return startPath + velocity * index *Time.fixedDeltaTime + GetFroceProjectile() *Time.fixedDeltaTime + projectTileGravityScale* Physics2D.gravity *index*index; //velocity *Time.fixedDeltaTime*index;
+    }
+}
